9568052eadeaad713581136ed2ad333206d4a832
tushcmd <muturidavid854@gmail.com>
2024-08-20 17:49:48 +0300

    styles: shadcn init

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,56 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { DM_Sans } from 'next/font/google'
+import { Space_Mono } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import './globals.css'
 
 export const metadata = {
   title: "RAG App",
   description: "RAG Customer Support App",
 };
 
+const fontHeading = DM_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  display: 'swap',
+  variable: '--font-heading',
+})
+
+const fontBody = Space_Mono({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
+  variable: '--font-body',
+})
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          'antialiased',
+          fontHeading.variable,
+          fontBody.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
-  );
+  )
 }
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata = {
+//   title: "RAG App",
+//   description: "RAG Customer Support App",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>{children}</body>
+//     </html>
+//   );
+// }
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between min-h-screen p-24 body-text">
       Dev
     </main>
   );
