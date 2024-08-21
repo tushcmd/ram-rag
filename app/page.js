2422ab0a84c445de546b7d5d9da6c0d4059085ee
tushcmd <muturidavid854@gmail.com>
2024-08-22 02:32:56 +0300

    fix: home button links

--- app/page.js
+++ app/page.js
@@ -8,15 +8,22 @@ export default function Home() {
     <main className="justify-center min-h-screen page-container">
       <h1 className='text-2xl '>Welcome to Rate a Mechanic App</h1>
       <div className='flex flex-col gap-2 md:flex-row md:space-x-2'>
-        <Button size="lg">
-          <Link href="/mechanics">List of Mechanics</Link>
-        </Button>
-        <Button size="lg">
-          <Link href="/chat">Go to Chat</Link>
-        </Button>
-        <Button size="lg">
-          <Link href="/submit-link">Submit Mechanic Profile Link</Link>
-        </Button>
+        <Link href="/mechanics">
+          <Button size="lg">
+            List of Mechanics
+          </Button>
+        </Link>
+
+        <Link href="/chat">
+          <Button size="lg">
+            Go to Chat
+          </Button>
+        </Link>
+        <Link href="/submit-link">
+          <Button size="lg">
+            Submit Mechanic Profile Link
+          </Button>
+        </Link>
       </div>
     </main>
   );
