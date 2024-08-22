38fed03bd7207f9822129d0900e3f51d80e854aa
tushcmd <muturidavid854@gmail.com>
2024-08-22 22:57:52 +0300

    feat: chat api route

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -3,24 +3,93 @@ import { NextResponse } from 'next/server';
 import OpenAI from 'openai';
 
 
-const systemPrompt = `You are a movie database assistant, helping users find information about movies stored in a Pinecone index.
-For every user question, retrieve the top 3 movies that best match the user's query. 
-Use these movies' details, such as titles, genres, actors, directors, and plot summaries, to answer the question.
- When a user asks a question, search the Pinecone movie index to retrieve the most relevant data. 
- Ensure that your responses are 
+// Defining systemPrompt
+const systemPrompt = `
+You are a rate my mechanic agent to help drivers find good garage shops, you take in user questions and answer them.
+For every user question, the top 3 mechanics that match the user question are returned.
+When a user asks a question, search the Pinecone movie index to retrieve the most relevant data. 
+Ensure that your responses are 
 concise, accurate, and directly related to the query. If additional context or clarification is needed, 
 ask follow-up questions to ensure you understand the user's intent before proceeding. Avoid speculation 
-and base your answers strictly on the data available in the index`
-
+and base your answers strictly on the data available in the index
+`
+// POST fn
 export async function POST(req) {
     const data = await req.json();
-    // return new Response(JSON.stringify(data))
+    // Initialize Pinecone and OpenAI
+    const pc = new Pinecone({
+        apiKey: process.env.PINECONE_API_KEY,
+    })
+
+    const index = pc.index('ram-index').namespace('ns1')
+    const openai = new OpenAI()
+
+    // Extract user's question & create embeddings
+    const text = data[data.length - 1].content
+    const embedding = await openai.embeddings.create({
+        model: 'text-embedding-3-small',
+        input: text,
+        encoding_format: 'float',
+    })
+
+    // Query pinecone
+    const results = await index.query({
+        topK: 5,
+        includeMetadata: true,
+        vector: embedding.data[0].embedding,
+    })
+
+
+    // Format results
+    let resultString = ''
+    results.matches.forEach((match) => {
+        resultString += `
+    Returned Results:
+    Mechanic:{match.id}
+    Name:{match.metadata.mechanicName}
+    Garage:{match.metadata.shopName}
+    Stars:{match.metadata.rating}
+    Review:{match.metadata.review}
+    Specialty:{match.metadata.specialty}
+    Location:{match.metadata.location}
+    \n\n`
+    })
+
+    const lastMessage = data[data.length - 1]
+    const lastMessageContent = lastMessage.content + resultString
+    const lastDataWithoutLastMessage = data.slice(0, data.length - 1)
+    // OpenAI Request
+    const completion = await openai.chat.completions.create({
+        messages: [
+            { role: 'system', content: systemPrompt },
+            ...lastDataWithoutLastMessage,
+            { role: 'user', content: lastMessageContent },
+        ],
+        model: 'gpt-3.5-turbo',
+        stream: true,
+    })
+
+    // Create Readable Stream
+    const stream = new ReadableStream({
+        async start(controller) {
+            const encoder = new TextEncoder()
+            try {
+                for await (const chunk of completion) {
+                    const content = chunk.choices[0]?.delta?.content
+                    if (content) {
+                        const text = encoder.encode(content)
+                        controller.enqueue(text)
+                    }
+                }
+            } catch (err) {
+                controller.error(err)
+            } finally {
+                controller.close()
+            }
+        },
+    })
+    return new NextResponse(stream)
 }
 
-const pc = new Pinecone({
-    apiKey: process.env.PINECONE_API_KEY,
-  })
-  const index = pc.index('ram-index').namespace('ns1')
-  const openai = new OpenAI()
 
-  
+
